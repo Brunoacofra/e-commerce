57e57fe3c5ad2037a7d9ad61f2ec5a1de359e342
Bruno <119612459+Brunoacofra@users.noreply.github.com>
2024-11-30 05:53:55 -0300

    Update compraClick.sql

--- compraClick.sql
+++ compraClick.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE compraClick;
-USE ecommerce;
+USE compraClick;
 -- Tabela de Usuários
 CREATE TABLE usuario (
  usu_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -28,24 +28,21 @@ CREATE TABLE ads (
 CREATE TABLE Perguntas (
  per_id INT AUTO_INCREMENT PRIMARY KEY,
  per_conteudo TEXT NOT NULL,
- per_ad_id INT NOT NULL,
+ per_ads_id INT NOT NULL,
  per_user_id INT NOT NULL,
  per_response TEXT,
  per_horario DATETIME,
- FOREIGN KEY (ad_id) REFERENCES ads(ads_id),
+ FOREIGN KEY (per_ads_id) REFERENCES ads(ads_id),
  FOREIGN KEY (per_user_id) REFERENCES usuario(usu_id)
 );
 -- Tabela de Compras
 CREATE TABLE compra (
  com_id INT AUTO_INCREMENT PRIMARY KEY,
- com_ad_id INT NOT NULL,
-FRAMEWORKS PARA DESENVOLVIMENTO
-DE SOFTWARE
-| 4º termo
+ com_ads_id INT NOT NULL,
  com_user_id INT NOT NULL,
  com_horario DATETIME ,
- FOREIGN KEY (ad_id) REFERENCES ads(ads_id),
- FOREIGN KEY (com_ad_id) REFERENCES usuario(usu_id)
+ FOREIGN KEY (com_ads_id) REFERENCES ads(ads_id),
+ FOREIGN KEY (com_user_id) REFERENCES usuario(usu_id)
 );
 -- Tabela de Favoritos
 CREATE TABLE favoritos (
@@ -53,5 +50,5 @@ CREATE TABLE favoritos (
  fav_user_id INT NOT NULL,
  fav_ad_id INT NOT NULL,
  FOREIGN KEY (fav_user_id) REFERENCES usuario(usu_id),
- FOREIGN KEY (fav_ad_id) REFERENCES ads(ads_id),
+ FOREIGN KEY (fav_ad_id) REFERENCES ads(ads_id)
 );
